62dbdf5344afb5ffb41547132b5ca535c216446c
Michael Timmons <michael.timmons@protonmail.com>
2020-05-28 22:44:29 -0400

    add ranges

---
+++ ranges/index.js
@@ -0,0 +1,13 @@
+const threeBet = require('./3bet')
+const fourBet = require('./4bet')
+const facingRaise = require('./facing_raise')
+const open = require('./open/index.js')
+const squeeze = require('./squeeze')
+
+module.exports = {
+   threeBet,
+   fourBet,
+   facingRaise,
+   open,
+   squeeze
+}
